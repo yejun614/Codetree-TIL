3b18ca35392d014849cacf94cb6ee754c57873fa
YeJun, Jung <31740224+yejun614@users.noreply.github.com>
2026-02-22 11:03:22 +0900

    Title: 행복한 수열의 개수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260222/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 260222/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,56 +1,85 @@
-#include <iostream>
-
+#include <cstdio>
 using namespace std;
 
+// ---------------------------------------------------------
+
+constexpr int MAX_BOARD_SIZE = 100;
+
 int answer;
-int n, m;
-int grid[100][100];
+int board_size;
+int seq_len;
+int board[MAX_BOARD_SIZE][MAX_BOARD_SIZE];
+
+// ---------------------------------------------------------
+
+inline void input() {
+    scanf("%d %d", &board_size, &seq_len);
+
+    for (int y = 0; y < board_size; ++y) {
+        for (int x = 0; x < board_size; ++x) {
+            scanf("%d", &board[y][x]);
+        }
+    }
+}
+
+// ---------------------------------------------------------
+
+void count_horizontal() {
+    for (int y = 0; y < board_size; ++y) {
+        int cnt = 1;
+        for (int x = 1; x < board_size; ++x) {
+            if (board[y][x - 1] == board[y][x]) {
+                if (++cnt == seq_len) {
+                    ++answer;
+                    break;
+                }
+            } else {
+                cnt = 1;
+            }
+        }
+    }
+}
+
+void count_vertical() {
+    for (int x = 0; x < board_size; ++x) {
+        int cnt = 1;
+        for (int y = 1; y < board_size; ++y) {
+            if (board[y - 1][x] == board[y][x]) {
+                if (++cnt == seq_len) {
+                    ++answer;
+                    break;
+                }
+            } else {
+                cnt = 1;
+            }
+        }
+    }
+}
+
+inline void solve() {
+    if (seq_len == 1) {
+        answer = 2 * board_size;
+        return;
+    }
+
+    count_horizontal();
+    count_vertical();
+}
+
+// ---------------------------------------------------------
+
+inline void print() {
+    printf("%d\n", answer);
+}
+
+// ---------------------------------------------------------
 
 int main() {
-    cin >> n >> m;
+    setbuf(stdout, NULL);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> grid[i][j];
-        }
-    }
-
-    if (m == 1) {
-        cout << 2 * n << "\n";
-        return 0;
-    }
-
-    answer = 0;
-
-    for (int y = 0; y < n; ++y) {
-        int cnt = 1;
-        for (int x = 1; x < n; ++x) {
-            if (grid[y][x - 1] == grid[y][x]) {
-                if (++cnt == m) {
-                    ++answer;
-                    break;
-                }
-            } else {
-                cnt = 1;
-            }
-        }
-    }
-
-    for (int x = 0; x < n; ++x) {
-        int cnt = 1;
-        for (int y = 1; y < n; ++y) {
-            if (grid[y - 1][x] == grid[y][x]) {
-                if (++cnt == m) {
-                    ++answer;
-                    break;
-                }
-            } else {
-                cnt = 1;
-            }
-        }
-    }
-
-    cout << answer << "\n";
+    input();
+    solve();
+    print();
 
     return 0;
 }
